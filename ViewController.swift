f73199819091553d9f178a17897d575e69a69ad5
Chaabane <mohamed.chaabane@etu.univ-amu.fr>
2020-01-27 12:01:02 +0100

    Init
    Creation du projet + .gitignore

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandoExpress_IOS
+//
+//  Created by etudiant-mac-02 on 27/01/2020.
+//  Copyright © 2020 fr.luminy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
