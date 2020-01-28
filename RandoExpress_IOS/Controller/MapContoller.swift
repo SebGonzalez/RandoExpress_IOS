5ed3b3c183947651a386274be41181128e63168c
Chaabane <mohamed.chaabane@etu.univ-amu.fr>
2020-01-28 17:27:10 +0100

    p

--- RandoExpress_IOS/Controller/MapContoller.swift
+++ RandoExpress_IOS/Controller/MapContoller.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 fr.luminy. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MapKit
+
+class MapController : UIViewController, MKMapViewDelegate{
+    
+    @IBOutlet weak var mapKit : MKMapView!
+    
+    override func viewDidLoad() {
+        
+        let citeU = Poi(title: "CiteU luminy", coordinate: CLLocationCoordinate2D(latitude: 43.229490, longitude: 5.438496), info: "Cite universitaire")
+        let tortuguero = Poi(title: "Le Redon", coordinate: CLLocationCoordinate2D(latitude: 43.245202, longitude: 5.426947), info: "Arret de bus du Redon")
+        
+        super.viewDidLoad()
+    }
+    
+}
--- RandoExpress_IOS/Controller/Poi.swift
+++ RandoExpress_IOS/Controller/Poi.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2020 fr.luminy. All rights reserved.
 //
 
-import Foundation
+import MapKit
+import UIKit
+class Poi: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    init(title: String, coordinate: CLLocationCoordinate2D, info:
+        String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
