51b467012ee450b24457750b12f770c5cc61fece
Chaabane <mohamed.chaabane@etu.univ-amu.fr>
2020-01-29 12:28:43 +0100

    Map fini

--- RandoExpress_IOS/Controller/MapContoller.swift
+++ RandoExpress_IOS/Controller/MapContoller.swift
@@ -15,8 +15,8 @@ class MapController : UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var mapView : MKMapView!
     
     // San José Capitale du Costa Rica pour centrer la carte
-    var latitudeInit: Double = 9.998784
-    var longitudeInit: Double = -84.204007
+    var latitudeInit: Double = 43.232230
+    var longitudeInit: Double = 5.435990
     var coordinateInit :  CLLocationCoordinate2D {
         return CLLocationCoordinate2D(latitude: latitudeInit, longitude: longitudeInit)
     }
@@ -26,10 +26,97 @@ class MapController : UIViewController, MKMapViewDelegate, CLLocationManagerDele
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let span = MKCoordinateSpan(latitudeDelta: 3, longitudeDelta: 3)
+        let region = MKCoordinateRegion(center: coordinateInit, span: span)
+        mapView.showsUserLocation = true
+        mapView.setRegion(region, animated: true)
+        mapView.delegate = self
+        mapView.isRotateEnabled = true
+        
+        mapView.showsUserLocation = true
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        locationManager.delegate = self
+        locationManager.startUpdatingHeading()
+        
+        
+        // ajout repère
         let randos = RandoGestionnaire.shared().randos
         
         for r in randos {
             mapView.addAnnotation(Poi(title: r.name, coordinate: CLLocationCoordinate2D(latitude: (r.latitude as NSString).doubleValue, longitude: (r.longitude as NSString).doubleValue), info: r.description))
+        }
+        
+        let capitalArea = MKCircle(center: coordinateInit, radius: 5000) // rayon de 5 km
+        mapView.addOverlay(capitalArea)
+    }
+    
+    // enrichir les annotations
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? Poi else { return nil }
+        let identifier = "poi"
+        var view: MKMarkerAnnotationView
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+    }
+    
+    // placer le titre et l'info du Poi dans l'alerte
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let poi = view.annotation as? Poi else { return }
+        let placeName = poi.title
+        let placeInfo = poi.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @IBAction func ChangeMapTypeButton(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0 : mapView.mapType = MKMapType.standard
+        case 1 : mapView.mapType = .satellite
+        case 2 : mapView.mapType = .hybrid
+        default: break
+        }
+    }
+    
+    // appellé par le bouton "localise moi"
+    func setupMap(coordonnees: CLLocationCoordinate2D, myLat: Double, myLong: Double) {
+        let span = MKCoordinateSpan(latitudeDelta: myLat , longitudeDelta: myLong)
+        let region = MKCoordinateRegion(center: coordonnees, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    @IBAction func getPosition(_ sender: Any) {
+        print("getPosition")
+        if userPosition != nil {
+            setupMap(coordonnees: userPosition!.coordinate, myLat: 1, myLong: 1)
+        } else {
+            print("nil dans getPosition")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        mapView.camera.heading = newHeading.magneticHeading
+        mapView.setCamera(mapView.camera, animated: true)
+    }
+    
+    // si mise à jour des locations
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.count > 0 {
+            if let maPosition = locations.last {
+                userPosition = maPosition
+            }
         }
     }
 }
