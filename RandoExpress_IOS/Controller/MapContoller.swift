825b4784a98479d13528ff3c37a35d20c8ae589b
Chaabane <mohamed.chaabane@etu.univ-amu.fr>
2020-01-29 11:39:00 +0100

    La map remarche

--- RandoExpress_IOS/Controller/MapContoller.swift
+++ RandoExpress_IOS/Controller/MapContoller.swift
@@ -14,45 +14,22 @@ class MapController : UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     @IBOutlet weak var mapView : MKMapView!
     
-    // Luminy pour centrer la carte
-    var latitudeInit: Double = 43.232230
-    var longitudeInit: Double = 5.435990
+    // San José Capitale du Costa Rica pour centrer la carte
+    var latitudeInit: Double = 9.998784
+    var longitudeInit: Double = -84.204007
     var coordinateInit :  CLLocationCoordinate2D {
         return CLLocationCoordinate2D(latitude: latitudeInit, longitude: longitudeInit)
     }
-    
     let locationManager = CLLocationManager()
     var userPosition: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
-    }
-
-    func setup() {
-        setupMap(coordonnees: coordinateInit, myLat: 3, myLong: 3)
-        mapView.showsUserLocation = true
-        mapView.delegate = self
-        mapView.isRotateEnabled = true
+        
         let randos = RandoGestionnaire.shared().randos
         
         for r in randos {
             mapView.addAnnotation(Poi(title: r.name, coordinate: CLLocationCoordinate2D(latitude: (r.latitude as NSString).doubleValue, longitude: (r.longitude as NSString).doubleValue), info: r.description))
         }
-        
-        let capitalArea = MKCircle(center: coordinateInit, radius: 5000) // rayon de 5 km
-        mapView.addOverlay(capitalArea)
     }
-    
-    // appellé par le bouton "localise moi"
-    func setupMap(coordonnees: CLLocationCoordinate2D, myLat: Double, myLong: Double) {
-        let span = MKCoordinateSpan(latitudeDelta: myLat , longitudeDelta: myLong)
-        let region = MKCoordinateRegion(center: coordonnees, span: span)
-        mapView.setRegion(region, animated: true)
-    }
-    
-    
-   
-
-
 }
